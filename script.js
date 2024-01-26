71cc0500965659651ff78fae4f3305afd0b5dc16
Daurkhan <darkhdzhum@mail.ru>
2024-01-26 16:01:33 +0600

    Completed lesson07

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-let arr = [2546789n, 354678n, 453245667n, 35467890n, 345668798900n, 876543n, 3456576n];
-let fourOrTwo = [];
+const daysAndWeek = {
+    week: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
 
-function firstLetter() {
-    for (let i = 0; i < arr.length; i++) {
-        let firstdigit = arr[i].toString().slice(0, 1);
-        if (firstdigit == "2" || firstdigit == "4") {
-            fourOrTwo.push(arr[i]);
-        }
-    }
-    return fourOrTwo;
-}
+    getDays: function () {
+        const currentDate = new Date();
+        let currentDay = currentDate.getDay();
 
-function showNumbers() {
-
-    if (fourOrTwo.length > 0) {
-        console.log("2 or 4: " + fourOrTwo);
-    }
-}
-
-function isPrime(num) {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return num !== 1;
-}
-
-function showPrimes(max) {
-    for (let i = 2; i <= max; i++) {
-        if (isPrime(i)) {
-            console.log("Делители числа " + i + ": 1 и " + i);
+        for (let i = 0; i < this.week.length; i++) {
+            if (i <= 4) {
+                console.log(this.week[i]);
+            } else if (i >= 5) {
+                console.log(this.week[i].italics());
+            } else if (i === currentDay) {
+                console.log(this.week[i].bold());
+            }
         }
     }
 }
 
-
-firstLetter();
-showNumbers();
-showPrimes(100);
+daysAndWeek.getDays();
